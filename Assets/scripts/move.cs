a4e9d4add1accff3df989e43a40cabfcfa8d5d0f
Klaudia Rutkowska <rutkowskaklaudia731@gmail.com>
2020-11-17 16:23:31 +0100

    Added lab 2, 3 and meshroom project

---
+++ Assets/scripts/move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed = 10.0f;
+    //public float x = transform.position.x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(speed, 0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/rotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        transform.Rotate(0, 90, 0, Space.Self);
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed);
+        transform.Rotate(0, 90, 0, Space.Self);
+        transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        transform.Rotate(0, 90, 0, Space.Self);
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed);
+        transform.Rotate(0, 90, 0, Space.Self);
+    }
+}
